e06e25c508ff884b1e4ad15b011dd6e02cdfe318
Tawanda <ejmunongo@gmail.com>
2023-06-14 21:46:49 +0200

    initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,45 @@
+<div class="navbar">
+    <div class="navbar-start">
+        <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+            </label>
+            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>About Us</a></li>
+                <li>
+                    <a>Parent</a>
+                    <ul class="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                    </ul>
+                </li>
+                <li><a>Products</a></li>
+            </ul>
+        </div>
+        <a href="#" class="btn btn-ghost normal-case text-xl">
+            <img src="assets/images/2.png" alt="logo" class="h-[48px]">
+        </a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+            <li class="text-lg text-black"><a>About Us</a></li>
+            <li tabindex="0">
+                <details>
+                    <summary class="text-lg text-black">Parent</summary>
+                    <ul class="p-2">
+                        <li class="text-lg"><a>Submenu 1</a></li>
+                        <li class="text-lg"><a>Submenu 2</a></li>
+                    </ul>
+                </details>
+            </li>
+            <li class="text-lg text-black"><a>Item 3</a></li>
+        </ul>
+    </div>
+    <div class="navbar-end">
+        <button class="btn btn-primary">
+            Contact Us
+        </button>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADM Electrical</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.1.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-[#EDF2EA] min-h-screen min-w-screen">
+    <?php include "includes/navbar.php" ?>
+    <!-- Banner -->
+    <div id="banner" class="hero min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <div>
+                    <img src="assets/images/6.png" alt="logo" class="block mx-auto">
+                </div>
+                <h1 class="text-5xl font-bold py-8">Innovative Solutions Delivering Power to the People</h1>
+                <div class="flex items-center justify-around">
+                    <button class="btn btn-info">
+                        Learn More
+                    </button>
+                    <button class="btn btn-warning">
+                        Solar Solutions
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "includes/footer.php" ?>
+</body>
+
+</html>
